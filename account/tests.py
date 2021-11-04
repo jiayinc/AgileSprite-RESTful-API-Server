ea5c1f32500a3639fa754172be1c7a1c01bf615b
Jiayin Cai <jiayinc@student.unimelb.edu.au>
2021-11-05 06:08:43 +0800

    add account logout tests

--- account/tests.py
+++ account/tests.py
@@ -203,3 +203,47 @@ class AccountLoginTests(APITestCase):
         response = self.client.post(self.url, data, format='json')
         content = json.loads(response.content)
         self.assertEqual(content['code'], ACCOUNT_LOGIN_SUCCESS)
+
+class AccountLogoutTests(APITestCase):
+
+    def setUp(self):
+        self.url = reverse('account-logout')
+        self.user = User.objects.create_user(username='email@email.com',
+                                             password='passwordASD123',
+                                             email='email@email.com',
+                                             first_name='first',
+                                             last_name='last')
+        data = {
+            'email': 'email@email.com',
+            'password': 'passwordASD123'
+        }
+        response = self.client.post(reverse('account-login'), data, format='json')
+        content = json.loads(response.content)
+        self.token = content['token']
+
+    def test_wrong_1(self):
+        # no token
+        data = {
+            'random': '123'
+        }
+        response = self.client.post(self.url, data, format='json')
+        content = json.loads(response.content)
+        self.assertEqual(content['code'], ACCOUNT_TOKEN_ERROR)
+
+    def test_wrong_2(self):
+        # wrong token
+        data = {
+            'token': '123'
+        }
+        response = self.client.post(self.url, data, format='json')
+        content = json.loads(response.content)
+        self.assertEqual(content['code'], ACCOUNT_TOKEN_ERROR)
+
+    def test_correct_1(self):
+        # wrong token
+        data = {
+            'token': self.token
+        }
+        response = self.client.post(self.url, data, format='json')
+        content = json.loads(response.content)
+        self.assertEqual(content['code'], ACCOUNT_LOGOUT_SUCCESS)
