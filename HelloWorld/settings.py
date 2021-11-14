33d72c94d26c340048bddf93fcb0ba0f50cd10c6
NUOYANC <nuoyanc@student.unimelb.edu.au>
2021-11-15 07:30:22 +0800

    Use heroku database

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -85,13 +85,13 @@ WSGI_APPLICATION = 'HelloWorld.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -137,16 +137,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "account.ExtendedUser"
 
-in_heroku = False
-if 'DATABASE_URL' in os.environ:
-    in_heroku = True
-
-if in_heroku:
-    DATABASES = {'default': dj_database_url.config()}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+DATABASES = {'default': dj_database_url.config()}
+#
+# in_heroku = False
+# if 'DATABASE_URL' in os.environ:
+#     in_heroku = True
+#
+# if in_heroku:
+#     DATABASES = {'default': dj_database_url.config()}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
