6a4d01235fcf23f23102d7bd8dda2abe1e9ae069
lixuand@student.unimelb.edu.au <lixuand@student.unimelb.edu.au>
2021-11-21 22:27:13 +0800

    feat(test):add test for group & canlendar

--- group/tests.py
+++ group/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+import json
+from django.urls import reverse
+from common.code import *
+from account.models import ExtendedUser as User
+from rest_framework.test import APITestCase
+from .models import Group
+from .models import ContactGroup
 
-# Create your tests here.
+
+class GroupTests(APITestCase):
+    def setUp(self):
+       return
+
+    def tearDown(self):
+        return
+        
+    def test_create_group(self):
+        Group.objects.create(name="peiyaa", user_id="7")
+        group = Group.objects.get(name="peiyaa")
+        self.assertEqual(group.user_id, 7)
+
+    def test_delete_group(self):
+        Group.objects.create(name="daniel", user_id="1")
+        Group.objects.create(name="peiyaa", user_id="7")
+
+        group_list = Group.objects.all()
+        self.assertEqual(group_list.count(), 2)
+        Group.objects.get(name="daniel").delete()
+        self.assertEqual(group_list.count(), 1)
+
+    def test_add_contact(self):
+        ContactGroup.objects.create(contact_id=1, group_id=2)
+        contact_group = ContactGroup.objects.get(contact_id=1)
+        self.assertEqual(contact_group.group_id, 2)
+
+    def test_delete_contact(self):
+        ContactGroup.objects.create(contact_id=3, group_id=4)
+        contact_group_list = ContactGroup.objects.all()
+        self.assertEqual(contact_group_list.count(), 1)
+        ContactGroup.objects.get(contact_id = 3).delete()
+        self.assertEqual(contact_group_list.count(), 0)
+
+    def test_get_all_group(self):
+        Group.objects.create(name="peiyaa", user_id="7")
+        self.assertEqual(Group.objects.all().count(), 1)
--- mycalendar/tests.py
+++ mycalendar/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import json
+from django.urls import reverse
+from common.code import *
+from account.models import ExtendedUser as User
+from rest_framework.test import APITestCase
+from contact.models import Contact
+from mycalendar.models import Event
 
-# Create your tests here.
+
+class CalendarTests(APITestCase):
+    def setUp(self):
+       return
+
+    def tearDown(self):
+        return
+        
+    def test_create_event(self):
+        Event.objects.create(date = "2021-11-11", user_id='1', name="peiyaa", category="peiyaa", start_time="2021-11-11 10:10:00", end_time="2021-11-11 10:10:00", comments="peiyaa", related_people="peiyaa")
+        event_object = Event.objects.get(name="peiyaa")
+        self.assertEqual(event_object.user_id, '1')
+
+    def test_delete_event(self):
+        Event.objects.create(date = "2021-11-11", user_id='1', name="peiyaa", category="peiyaa", start_time="2021-11-11 10:10", end_time="2021-11-11 10:10", comments="peiyaa", related_people="peiyaa")
+
+        Event.objects.create(date = "2021-11-11", user_id='1', name="daniel", category="peiyaa", start_time="2021-11-11 10:10", end_time="2021-11-11 10:10", comments="peiyaa", related_people="peiyaa")
+        event_list = Event.objects.all()
+        self.assertEqual(event_list.count(), 2)
+        Event.objects.get(name="daniel").delete()
+        event_list = Event.objects.all()
+        self.assertEqual(event_list.count(), 1)
+
+    def test_update_event(self):
+        Event.objects.create(date = "2021-11-11", user_id=1, name="peiyaa", category="peiyaa", start_time="2021-11-11 10:10", end_time="2021-11-11 10:10", comments="peiyaa", related_people="peiyaa")
+        event = Event.objects.get(name="peiyaa")
+        event.category = "peiyaa_2"
+        event.save()
+        self.assertEqual(Event.objects.get(name="peiyaa").category, "peiyaa_2")
+    
+    def test_get_day_event(self):
+        Event.objects.create(date = "2021-11-11", user_id=1, name="daniel", category="peiyaa", start_time="2021-11-11 10:10", end_time="2021-11-11 10:10", comments="peiyaa", related_people="peiyaa")
+        Event.objects.create(date = "2021-11-11", user_id=1, name="daniel2", category="peiyaa", start_time="2021-11-11 10:10", end_time="2021-11-11 10:10", comments="peiyaa", related_people="peiyaa")
+        self.assertEqual(Event.objects.filter(date="2021-11-11").count(),2)
